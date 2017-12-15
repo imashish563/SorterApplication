39c8a1abf1f9912b1bd8fdfaa9107d376aa745fa
Ashish Goyal <34535449+imashish563@users.noreply.github.com>
2017-12-15 17:35:56 +0530

    Update SorterApplication.java

--- src/main/java/com/tcs/SorterApplication.java
+++ src/main/java/com/tcs/SorterApplication.java
@@ -2,8 +2,12 @@ package com.tcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -13,4 +17,15 @@ public class SorterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SorterApplication.class, args);
 	}
+	
+	@Bean
+    WebMvcConfigurer configurer () {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers (ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/resources/**").
+                          addResourceLocations("/resources/");
+            }
+        };
+    }
 }
